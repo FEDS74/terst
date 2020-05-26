0d56bf55c3c9f829986ab52073a5ca41f12603fd
Thomas Bach-Nilsen <55656461+FEDS74@users.noreply.github.com>
2020-05-26 23:22:59 +0200

    sdfghj

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+
+function showFilter(){
+  var openFilter = document.querySelector(".type_container");
+  if (openFilter.style.display === "none") {
+    openFilter.style.display == "block";
+  }else {
+    openFilter.style.display == "none";
+  }
+}
